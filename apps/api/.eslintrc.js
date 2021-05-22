75f8d4be31c95bee52e02a4f1735c99dda7d1bb0
Giovane Mariel <giovanemariel@gmail.com>
2021-05-22 20:42:08 -0300

    Projeto criado

---
+++ apps/api/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    // 'plugin:@typescript-eslint/recommended',
+  ],
+  // parser: '@typescript-eslint/parser',
+  parserOptions: {
+    // ecmaFeatures: {
+    //   jsx: true,
+    // },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  // plugins: ['@typescript-eslint'],
+  rules: {
+    // '@typescript-eslint/ban-types': 0,
+    // '@typescript-eslint/no-explicit-any': 0,
+  },
+}
